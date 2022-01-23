41e0657196e06dcaf485eece3d74d390b63d443a
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-01-22 23:16:19 -0300

    Projeto juros entre países

---
+++ src/service/USAInterestService.java
@@ -0,0 +1,18 @@
+package service;
+
+public class USAInterestService implements InterestService{
+	
+	private double interestRate;
+
+	public USAInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
+
+
